e7074e1664ab93c473ecb3f6464f3e12027253d5
Murilo Roque <muriloengqui@gmail.com>
2020-06-09 05:41:59 -0300

    implement object to show on front-end

--- scripts.js
+++ scripts.js
@@ -18,3 +18,13 @@ bookDetails.title = prompt("Choose a book title");
 bookDetails.pages = prompt("Choose the number of pages");
 bookDetails.read = prompt("Was it read by you?");
 addBookToLibrary(bookDetails);
+
+const container = document.getElementById("books");
+
+for(let i = 0; i < myLibrary.length; i++){
+  let cell = document.createElement("div");
+  container.appendChild(cell).className = "book-div";
+  let book = document.createElement("p");
+  cell.appendChild(book);
+  book.textContent = myLibrary[i].author + myLibrary[i].title + myLibrary[i].pages + myLibrary[i].read;
+}
