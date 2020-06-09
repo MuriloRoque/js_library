f3fa4281ad6d960c1dacd6959a66b110669a8ab8
Murilo Roque <muriloengqui@gmail.com>
2020-06-09 11:28:43 -0300

    fix remove button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 let myLibrary = [];
+let count = 0;
 
-function Book(author, title, pages, read) {
+function Book(id, author, title, pages, read) {
+  this.id = id;
   this.author = author;
   this.title = title;
   this.pages = pages;
@@ -8,13 +10,15 @@ function Book(author, title, pages, read) {
 }
 
 function addBookToLibrary(bookDetails) {
-  let book = new Book(bookDetails.author, bookDetails.title, bookDetails.pages, bookDetails.read);
+  let book = new Book(bookDetails.id, bookDetails.author, bookDetails.title, bookDetails.pages, bookDetails.read);
   myLibrary.push(book);
 }
 
 const bookDetails = {}
 
 document.querySelector("#action").onsubmit = function(){
+  count++;
+  bookDetails.id = count;
   bookDetails.author = document.querySelector("#author").value;
   bookDetails.title = document.querySelector("#title").value;
   bookDetails.pages = document.querySelector("#pages").value;
@@ -33,15 +37,24 @@ document.querySelector("#action").onsubmit = function(){
 const container = document.querySelector("#books");
 
 function printLibrary(){
-  let cell = document.createElement("div");
-  container.appendChild(cell).className = "book-div";
-  let book = document.createElement("p");
-  cell.appendChild(book);
-  book.textContent = myLibrary[myLibrary.length-1].author + myLibrary[myLibrary.length-1].title 
-  + myLibrary[myLibrary.length-1].pages + myLibrary[myLibrary.length-1].read;
+  let book = document.createElement("div");
+  container.appendChild(book).className = "book-div";
+  let author = document.createElement("p");
+  let title = document.createElement("p");
+  let pages = document.createElement("p");
+  let read = document.createElement("p");
+  book.appendChild(author);
+  book.appendChild(title);
+  book.appendChild(pages);
+  book.appendChild(read);
+  author.textContent = myLibrary[myLibrary.length-1].author;
+  title.textContent = myLibrary[myLibrary.length-1].title;
+  pages.textContent = myLibrary[myLibrary.length-1].pages;
+  read.textContent = myLibrary[myLibrary.length-1].read;
   let button = document.createElement("button");
   button.textContent = 'X';
   button.setAttribute('class','remove');
+  button.setAttribute('value', myLibrary[myLibrary.length-1].id);
   book.appendChild(button);
   removeBook();
 }
@@ -51,6 +64,7 @@ function removeBook(){
     let buttons = document.querySelectorAll('.remove');
     buttons.forEach(function(element){
         element.addEventListener('click',function(){
+            delete myLibrary[element.value-1];
             element.parentElement.remove();
           }  
         )
