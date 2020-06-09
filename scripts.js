0ea24b4d65e3632fe22171b28894dfaf3ef03099
Murilo Roque <muriloengqui@gmail.com>
2020-06-09 06:43:21 -0300

    integrate form inputs showing on page

--- scripts.js
+++ scripts.js
@@ -13,20 +13,26 @@ function addBookToLibrary(bookDetails) {
 }
 
 const bookDetails = {}
-// bookDetails.author = prompt("Choose a book author");
-// bookDetails.title = prompt("Choose a book title");
-// bookDetails.pages = prompt("Choose the number of pages");
-// bookDetails.read = prompt("Was it read by you?");
-addBookToLibrary(bookDetails);
+
+document.querySelector("#action").onsubmit = function(){
+  bookDetails.author = document.querySelector("#author").value;
+  bookDetails.title = document.querySelector("#title").value;
+  bookDetails.pages = document.querySelector("#pages").value;
+  bookDetails.read = document.querySelector("#read").value;
+  addBookToLibrary(bookDetails);
+  printLibrary();
+}
 
 const container = document.querySelector("#books");
 
-for(let i = 0; i < myLibrary.length; i++){
-  let cell = document.createElement("div");
-  container.appendChild(cell).className = "book-div";
-  let book = document.createElement("p");
-  cell.appendChild(book);
-  book.textContent = myLibrary[i].author + myLibrary[i].title + myLibrary[i].pages + myLibrary[i].read;
+function printLibrary(){
+  for(let i = 0; i < myLibrary.length; i++){
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "book-div";
+    let book = document.createElement("p");
+    cell.appendChild(book);
+    book.textContent = myLibrary[i].author + myLibrary[i].title + myLibrary[i].pages + myLibrary[i].read;
+  }
 }
 
 const addNew = document.querySelector("#add-new");
